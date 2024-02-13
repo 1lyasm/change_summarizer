4d482016dbf0cf688006ba5cdad6f65b3e0c9c6d
1lyasm <1lyasm.dev@gmail.com>
2024-02-13 17:42:58 +0400

    Parse ncol, nrow, and maxval from PPM

--- src/main.cpp
+++ src/main.cpp
@@ -8,29 +8,66 @@
 #include <sstream>
 #include <string>
 
-int main() {
-    std::ifstream ifile("data/tree_2.ppm", std::ios::binary);
-
-    std::basic_ostringstream<char> ostr;
-    ostr << ifile.rdbuf();
-
-    const std::string &&imref = ostr.rdbuf()->str();
-    std::size_t imlen = imref.size();
-    unsigned char *im = static_cast<unsigned char*>(malloc((imlen + 1) * sizeof(unsigned char)));
-    if (im == nullptr) {
-        fprintf(stderr, "%s: malloc failed\n", __func__);
-        exit(EXIT_FAILURE);
-    }
-
-    for (unsigned i = 0; i < imlen; ++i) {
-        im[i] = static_cast<unsigned char>(imref.data()[i]);
-    }
-    im[imlen] = 0;
-
-    for (unsigned i = 0; i < imlen; ++i) {
-        std::cout << std::to_string(im[i]) << "\n";
-    }
-
-    free(im);
+static void expect(unsigned char val, unsigned char targ) {
+  if (val != targ) {
+    fprintf(stderr, "%s: expected '%c', found '%c'\n", __func__, targ, val);
+    exit(EXIT_FAILURE);
+  }
 }
 
+static unsigned parse_num(unsigned char *im, unsigned &idx,
+                          unsigned char delim) {
+  unsigned num = 0, ndigit = 0, coef = 1;
+
+  ++idx;
+  for (; im[idx + ndigit] != delim; ++ndigit)
+    ;
+
+  for (unsigned i = ndigit - 1, j = 0; j < ndigit; --i, ++j) {
+    num += coef * (im[idx + i] - '0');
+    coef *= 10;
+  }
+
+  idx += ndigit;
+  expect(im[idx], delim);
+
+  return num;
+}
+
+int main() {
+  std::ifstream ifile("data/tree_2.ppm", std::ios::binary);
+
+  std::basic_ostringstream<char> ostr;
+  ostr << ifile.rdbuf();
+
+  const std::string &&imref = ostr.rdbuf()->str();
+  std::size_t imlen = imref.size();
+  unsigned char *im =
+      static_cast<unsigned char *>(malloc((imlen + 1) * sizeof(unsigned char)));
+  if (im == nullptr) {
+    fprintf(stderr, "%s: malloc failed\n", __func__);
+    exit(EXIT_FAILURE);
+  }
+
+  for (unsigned i = 0; i < imlen; ++i) {
+    im[i] = static_cast<unsigned char>(imref.data()[i]);
+  }
+  im[imlen] = 0;
+
+  unsigned idx = 0;
+  expect(im[idx], 'P');
+  expect(im[++idx], '6');
+  expect(im[++idx], '\n');
+  unsigned ncol = parse_num(im, idx, ' ');
+  unsigned nrow = parse_num(im, idx, '\n');
+  unsigned maxval = parse_num(im, idx, '\n');
+
+  std::cout << "ncol: " << ncol << ", nrow: " << nrow << ", maxval: "
+            << maxval << "\n";
+
+  /* for (unsigned i = 0; i < imlen; ++i) { */
+  /*   std::cout << std::to_string(im[i]) << "\n"; */
+  /* } */
+
+  free(im);
+}
